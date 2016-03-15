9231b15f5d0bb9544863fd1bd601f26e1faefcaf
liukaitc <liukaitc@gmail.com>
2016-03-14 20:03:10 -0400

    kai change testimonial

--- includes/testimonial.php
+++ includes/testimonial.php
@@ -6,22 +6,23 @@
 <div class="testimonial">
 	<div class="right">
 		<p>
-		Kai Liu is a gifted photographer. His depth of experience, creative insight, lighting expertise and compositions all applied to the new digital photography. 
-		Kai has provided beautiful images of our extensive food menu which was later used for the restaurant's website. You can find them at turkishkitchen.com
-		We have been using Mr. Liu services for our private and cooperative events. His photography captures the fun and enthusiasm of our customers as they enter into the spirit of the party by recording their spontaneous smiles, laughter and sheer enjoyment of our special events. 
+		I have had the pleasure of working with Kai Liu on multiple projects over the past two years. 
+		He is truly a pleasure to work with and he is such an incredibly multi-faceted photographer, 
+		he has become my go-to for anything and everything regarding photography. 
+		He excels with architectural images and captures the essence of any space that he photographs. 
+		Amazingly he is equally gifted as an event photographer and always seems to get the perfect action shot of any large scale event or production I put on. 
+		He always edits and presents all of his work beautifully and always brings out the best in any subject he photographs. 
+		I am looking forward to working with Kai for the rest of my career and will be a dedicated client for life and am looking forward to working with him on all of my future projects. 
 		</p>
 		<p class="signiture">
 			<br/>
-			Best Wishes<br/>
-			Turkish Kitchen
+			Sincerely,
+			<br/>
+			Evan Hungate
+			<br/>
+			Director of Sales & Events
 		</p>
 	</div>
-	<div class="left">
-		<img src="https://www.turkishkitchen.com/webcoreimages/s_menu_logo.jpg" border="0"/>
-		<p><a href="//www.turkishkitchen.com/" target="_blank"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> website </a></p>
-	</div>
-	<div class="clear"></div>
-</div>
 
 <div class="divider"></div>	
 
@@ -50,19 +51,23 @@
 <div class="testimonial">
 	<div class="right">
 		<p>
-		Kai Liu is a gifted photographer. His depth of experience, creative insight, lighting expertise and compositions all applied to the new digital photography. 
-		Kai has provided beautiful images of our extensive food menu which was later used for the restaurant's website. You can find them at turkishkitchen.com
-		We have been using Mr. Liu services for our private and cooperative events. His photography captures the fun and enthusiasm of our customers as they enter into the spirit of the party by recording their spontaneous smiles, laughter and sheer enjoyment of our special events. 
+		Kai was a very proactive and tireless contributor to the Voovio New York project. 
+		His high standard of photography was matched by his willingness to go the extra mile for a photoshoot. 
+		I was particularly impressed by his ability to balance professionalism with adding his own creative spark.
+		I enjoyed working with Kai not just for his skill and experience, but because of his personable nature and unchallenged work ethic, which ensured that my expectations were exceeded consistently.
 		</p>
 		<p class="signiture">
 			<br/>
-			Best Wishes<br/>
-			Turkish Kitchen
+			Best Wishes
+			<br/>
+			Nick Tinker
+			<br/>
+			Sale Manager
 		</p>
 	</div>
 	<div class="left">
-		<img src="https://www.turkishkitchen.com/webcoreimages/s_menu_logo.jpg" border="0"/>
-		<p><a href="//www.turkishkitchen.com/"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> website </a></p>
+		<img src="images/testimonial-1.jpg" border="0"/>
+		<p><a href="//www.voovio.com/"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> website </a></p>
 	</div>
 	<div class="clear"></div>
 </div>
@@ -72,19 +77,47 @@
 <div class="testimonial">
 	<div class="right">
 		<p>
-		Kai Liu is a gifted photographer. His depth of experience, creative insight, lighting expertise and compositions all applied to the new digital photography. 
-		Kai has provided beautiful images of our extensive food menu which was later used for the restaurant's website. You can find them at turkishkitchen.com
-		We have been using Mr. Liu services for our private and cooperative events. His photography captures the fun and enthusiasm of our customers as they enter into the spirit of the party by recording their spontaneous smiles, laughter and sheer enjoyment of our special events. 
+		Kai Liu can be described in three words: Profressional, Talented, and Determined. 
+		I had a project that had many challenges due to the nature of my frozen food products and Kai Liu handled them with creativity and ease. 
+		He made me feel very comfortable while working with him. He stuck to my budget and the photos are top notch. 
+		I will definitely be using him again in the future. 
 		</p>
 		<p class="signiture">
 			<br/>
-			Best Wishes<br/>
-			Turkish Kitchen
+			Regards,
+			<br/>
+			Malcolm Paulino
 		</p>
 	</div>
 	<div class="left">
-		<img src="https://www.turkishkitchen.com/webcoreimages/s_menu_logo.jpg" border="0"/>
-		<p><a href="//www.turkishkitchen.com/"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> website </a></p>
+		<img src="images/testimonial-3.jpg" border="0"/>
+		<p><a href="//www.vrillfood.com/"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> website </a></p>
+	</div>
+	<div class="clear"></div>
+</div>
+
+</article>
+<div class="clear"></div>
+<div class="divider"></div>
+
+<div class="testimonial">
+	<div class="right">
+		<p>
+		Working with Kai Liu was a great experience.  
+		Kai was hired to work on photography for our product packaging.  
+		He worked seamlessly with our designer, making for a very efficient and successful photo shoot.  
+		The final packaging came out beautifully, and needless to say, a direct result of Kai's abilities.  
+		I look forward to a continued working relationship with Kai, and would recommend him to anyone needing a true professional.
+		</p>
+		<p class="signiture">
+			<br/>
+			Gino Colangelo
+			<br/>
+			Japalia Foods
+		</p>
+	</div>
+	<div class="left">
+		<img src="images/testimonial-4.jpg" border="0"/>
 	</div>
 	<div class="clear"></div>
 </div>
