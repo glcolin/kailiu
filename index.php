8343ffd861237230cd709d0dc7c578ae3a23fc33
Colin <glcolin@hotmail.com>
2016-03-15 21:02:03 -0400

    added input for keywords in index.php and taught kai how to do backlinks

--- index.php
+++ index.php
@@ -9,37 +9,72 @@ if(isset($_GET['p'])){
 switch($page){
 
 	case 'index':
+		//BEGIN:::
+		$title = 'The most affordable food photographer in NYC';
+		$description = 'I am a New York based freelance photographer/videographer. I spealize in food photography. I am also doing video for event, shows, wedding, music video and commercial video.';
+		$keywords = 'food photographer, low cost food photographer, ny food photographer, ny food photography';
+		//:::END
 		$parts = array(
 				'whatsinkai','portfolio','highlight','about','clients'
 			);
 		break;
 	case 'gallery':
+		//BEGIN:::
+		$title = '';
+		$description = '';
+		$keywords = '';
+		//:::END
 		$parts =array(
 				'portfolio','gallery','about','clients'
 			);
 		$type = $_GET['type'];
 		break;
 	case 'about':
+		//BEGIN:::
+		$title = 'About Kai Liu';
+		$description = 'I am a New York based freelance photographer/videographer. I spealize in food photography. I am also doing video for event, shows, wedding, music video and commercial video.';
+		$keywords = 'food photographer, low cost food photographer';
+		//:::END
 		$parts =array(
 				'portfolio','aboutkai','whatsinkai','clients'
 			);
 		break;
 	case 'contact':
+		//BEGIN:::
+		$title = '';
+		$description = '';
+		$keywords = '';
+		//:::END
 		$parts =array(
 				'portfolio','contact','about','clients'
 			);
 		break;
 	case 'testimonial':
+		//BEGIN:::
+		$title = '';
+		$description = '';
+		$keywords = '';
+		//:::END
 		$parts =array(
 				'portfolio','testimonial','about','clients'
 			);
 		break;
 	case 'services':
+		//BEGIN:::
+		$title = '';
+		$description = '';
+		$keywords = '';
+		//:::END
 		$parts =array(
 				'portfolio','services','whatsinkai','clients'
 			);
 		break;
 	case 'blog':
+		//BEGIN:::
+		$title = '';
+		$description = '';
+		$keywords = '';
+		//:::END
 		$parts =array(
 				'portfolio','blog','whatsinkai','clients'
 			);
@@ -50,6 +85,11 @@ switch($page){
 			);
 		break;
 	case 'video':
+		//BEGIN:::
+		$title = '';
+		$description = '';
+		$keywords = '';
+		//:::END
 		$parts =array(
 				'portfolio','video','about','clients'
 			);
--- layout/template.php
+++ layout/template.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title><?php echo $title;?></title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<meta name="keywords" content="">
+	<meta name="description" content="<?php echo $description;?>">
+	<meta name="keywords" content="<?php echo $keywords;?>">
 	<meta name="author" content="wwww.369usa.com">
 	<meta name="format-detection" content="telephone=no">
 	<!--Load bootstrap library and JQuery-->
