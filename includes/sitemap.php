344e2a1e73106b2682cbc4cbb4c8c60791d4cf46
Colin <glcolin@hotmail.com>
2016-05-05 13:20:50 -0400

    init sitemao

---
+++ includes/sitemap.php
@@ -0,0 +1,12 @@
+<div class="gallery col-md-8 col-sm-12">	
+<div class="tablet-only title highlight">Sitemap</div>
+<div class="divider"></div>	
+<article class="kai">
+		
+		
+	
+</article>
+<div class="clear"></div>
+<div class="divider"></div>
+<p>&nbsp;</p>
+</div>
