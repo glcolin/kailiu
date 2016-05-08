c36abd8c6888eec68c81d3043e64851a6693706c
kai liu <kai liu>
2016-05-08 19:21:17 -0400

    kai update

--- includes/sitemap.php
+++ includes/sitemap.php
@@ -14,18 +14,22 @@
 				<li><a href="video">Video</a></li>
 			</ul>
 		</li>
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Home</a></li>
+		<li><a href="about">About</a></li>
+		<li><a href="testimonials">Testimonials</a></li>
+		<li><a href="service-and-quotes">Service and Quotes</a></li>
 		<li>
-			<a href="#">Blog</a>
+			<a href="blog">Blog</a>
 			<ul>
-				<li><a href="food-photography">Food Photography</a></li>
-				<li><a href="interior-photography">Interior Photography</a></li>
-				<li><a href="people-photography">Event Photography</a></li>
-				<li><a href="video">Video</a></li>
+				<li><a href="food-10001-Food-photography-with-human-subject">Food photography with human subject</a></li>
+				<li><a href="food-10002-Food-Photography-for-Packaging-how-to-shoot-subject-on-pure-white-background">Food Photography for Packaging: how to shoot subject on pure white background</a></li>
+				<li><a href="food-10003-Food-photography-look-boring-without-propping-and-art-director">Food photography look boring without propping and art director</a></li>
+				<li><a href="food-10004-Commercial-Video-for-Kulu-Dessert-A-new-concept-to-promote-restaurants">Commercial Video for Kulu Dessert. A new concept to promote restaurants</a></li>
+				<li><a href="food-10005-Where-to-Eat-in-NYC-My-New-Video-Project">Where to eat in NYC - my new video project</a></li>
+				<li><a href="food-10006-How-Much-Photographer-Should-Charge">The reality of how much photographer should charge</a></li>
+				<li><a href="food-10007-Food-Photography-Chinese-Burger">Food Photography - Chinese Burger</a></li>
 			</ul>
 		</li>
+		<li><a href="contact">Contact Us</a></li>
 		</ul>
 	</div>	
 	
